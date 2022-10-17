4d9faa6bbf0af9b30a2f3300959db077478d8c4a
diaken2 <107970623+diaken2@users.noreply.github.com>
2022-10-17 17:56:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const mongoose=require('mongoose')
 const User=require("./model/User")
 const path=require("path")
 const PORT=process.env.PORT || 5000
+app.use(function(req, res, next) {
 
+
+  res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  
+
+  
+  });
 app.use(express.json({extended:true}))
 
 const start=async() =>{
